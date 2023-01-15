f434a6f7d958e5c955e5a062b46b82c9f0c989f2
Exoutia <jhav365@gmail.com>
2023-01-15 15:30:24 +0530

    adding the dark mode for  myself.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
 
 function App() {
+  let myBodyStyle = {
+    color: "white",
+    backgroundColor: "#212529",
+  }
   return (
-    <>
+    <div style={myBodyStyle}>
       <Navbar title="Text Utils" aboutText="About us" />
       <div className="container">
       <TextForm heading="Enter the text to analyze." />
       </div>
-    </>
+      <About></About>
+    </div>
   );
 }
 
