c052150253b1422883d3a4b15ac3f6126db46579
Exoutia <jhav365@gmail.com>
2023-01-15 15:53:47 +0530

    adding the delete extra spaces.

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -22,11 +22,24 @@ export default function TextForm(props) {
     let newText = '';
     setText(newText);
   }
-  
+
 
   const handleOnChange = (event) => {
     console.log("this hdnale on change");
     setText(event.target.value); // this is the only way to change the value.
+  }
+
+  const handleCopyOnChange = () => {
+    console.log("I am copy");
+    let text = document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  }
+
+  const handleExtraWhiteSpace = () => {
+    console.log("remove extra spaces");
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "))
   }
 
   const [text, setText] = useState("Enter text here");
@@ -37,10 +50,13 @@ export default function TextForm(props) {
       <div className="mb-3">
         <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
       </div>
-      <button className="btn btn-primary mx-2" onClick={handleUpClick} >Convert to Uppercase </button>
-      <button className="btn btn-primary mx-2" onClick={handleLoClick} >Convert to Lowercase </button>
-      <button className="btn btn-primary mx-2" onClick={handleInverseClick} >Convert to Inverse </button>
-      <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear</button>
+      <button className="btn btn-primary mx-2 my-1" onClick={handleUpClick} >Convert to Uppercase </button>
+      <button className="btn btn-primary mx-2 my-1" onClick={handleLoClick} >Convert to Lowercase </button>
+      <button className="btn btn-primary mx-2 my-1" onClick={handleInverseClick} >Convert to Inverse </button>
+      <button className="btn btn-primary mx-2 my-1" onClick={handleClearClick}>Clear</button>
+      <button className="btn btn-primary mx-2 my-1" onClick={handleCopyOnChange}>copy</button>
+      <button className="btn btn-primary mx-2 my-1" onClick={handleExtraWhiteSpace}>remove extra spaces</button>
+
     </div>
     <div className="container">
       <h2>Your text summary</h2>
