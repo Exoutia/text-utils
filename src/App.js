1a90b329057126ff1a0bf4073157d1f54af96a77
Exoutia <jhav365@gmail.com>
2023-01-17 17:55:04 +0530

    removing the dark mode.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 // import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import About from './components/About';
 
 function App() {
-  let myBodyStyle = {
-    color: "white",
-    backgroundColor: "#212529",
+  let [mode, setMode] = useState("primary");
+
+  let toggleMode = () => {
+    if (mode === 'primary'){
+      setMode('dark')
+      document.body.style.backgroundColor = "#414141"
+      // document.
+    }
+    else{
+      setMode('primary')
+      document.body.style.backgroundColor = "white"
+    }
   }
+
   return (
-    <div style={myBodyStyle}>
-      <Navbar title="Text Utils" aboutText="About us" />
+    <div>
+      <Navbar title="Text Utils" aboutText="About us" mode={mode} toggleMode={toggleMode} />
       <div className="container">
-      <TextForm heading="Enter the text to analyze." />
+      <TextForm heading="Enter the text to analyze." mode={mode}/>
       </div>
-      <About></About>
+      {/* <About mode={mode} ></About> */}
     </div>
   );
 }
