1d4403f2d125dd35dc36f8752651c0f445007602
Exoutia <jhav365@gmail.com>
2023-01-18 17:54:41 +0530

    adding teh alert props into the app

--- src/App.js
+++ src/App.js
@@ -3,28 +3,44 @@ import './App.css';
 import { useState } from 'react';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-import About from './components/About';
+// import About from './components/About';
+import Alert from './components/Alert';
 
 function App() {
   let [mode, setMode] = useState("primary");
+
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 4000);
+  }
 
   let toggleMode = () => {
     if (mode === 'primary'){
       setMode('dark')
       document.body.style.backgroundColor = "#414141"
-      // document.
+      showAlert("Dark Mode Enabled", "success")
     }
     else{
       setMode('primary')
       document.body.style.backgroundColor = "white"
+      showAlert("Light Mode Enabled", "success")
+
     }
   }
 
   return (
     <div>
       <Navbar title="Text Utils" aboutText="About us" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert}/>
       <div className="container">
-      <TextForm heading="Enter the text to analyze." mode={mode}/>
+      <TextForm showAlert={showAlert} heading="Enter the text to analyze." mode={mode}/>
       </div>
       {/* <About mode={mode} ></About> */}
     </div>
